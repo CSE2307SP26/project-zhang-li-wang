64fb2b6cfde1cb2133818824b80bf333206b5546
WangTianwei-w <tianwei@wustl.edu>
2026-03-25 23:53:17 -0500

    unified menu order

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -17,10 +17,15 @@ public class MainMenu {
 
     public void displayOptions() {
         System.out.println("Welcome to the 237 Bank App!");
-
-        System.out.println("1. Make a deposit");
-        System.out.println("2. Check balance");
-        System.out.println("3. Exit the app");
+        System.out.println("1. Deposit into an existing account");
+        System.out.println("2. Withdraw from an account");
+        System.out.println("3. Check account balance");
+        System.out.println("4. Create an additional account");
+        System.out.println("5. Close an existing account");
+        System.out.println("6. Transfer money between accounts");
+        System.out.println("7. Add interest payment (Admin)");
+        System.out.println("8. Exit the app");
+}
     }
 
     public int getUserSelection(int max) {
@@ -34,12 +39,27 @@ public class MainMenu {
 
     public void processInput(int selection) {
         switch (selection) {
-            case 1:
-                performDeposit();
-                break;
-            case 2:
-                checkBalance();
-                break;
+        case 1:
+            performDeposit();
+            break;
+        case 2:
+            performWithdraw();
+            break;
+        case 3:
+            checkBalance();
+            break;
+        case 4:
+            createAccount();
+            break;
+        case 5:
+            closeAccount();
+            break;
+        case 6:
+            performTransfer();
+            break;
+        case 7:
+            addInterest();
+            break;
 
         }
     }
